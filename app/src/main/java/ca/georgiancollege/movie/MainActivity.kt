54cf7e05ca80a14c5d255f9f87f64d59d0a200c0
JouChun <aafree2014@gmail.com>
2025-07-24 21:53:13 -0400

    update main binding and search button function

--- app/src/main/java/ca/georgiancollege/movie/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/movie/MainActivity.kt
@@ -1,25 +1,37 @@
 package ca.georgiancollege.movie
 
+import MovieAdapter
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import ca.georgiancollege.movie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var movieAdapter: MovieAdapter
+    private val movies = mutableListOf<Movie>()
+
+    private val apiKey = "aeb47a88"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         enableEdgeToEdge()
 
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding.buttonSearch.setOnClickListener {
+
+            val query = binding.editTextSearch.text.toString()
+            if (query.isNotBlank()) {
+                searchResults(query)
+            } else {
+                Toast.makeText(this, "Enter the keyword...", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    private fun searchResults(query: String) {
     }
 }
